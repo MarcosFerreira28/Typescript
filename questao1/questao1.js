4fb641d290db2b148f6f9ff9f21902c68299cdab
Marcos <mvmendesferreira@id.uff.br>
2025-08-02 13:06:31 -0300

    add: adicionando questão 1 mas com erro de tipo nao implicito

---
+++ questao1/questao1.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function adicionar(a, b) {
+    return a + b;
+}
+let total = 100;
+const resultado = adicionar(20, 40);
+console.log(resultado);
+//# sourceMappingURL=index.js.map
---
+++ questao1/questao1.ts
@@ -0,0 +1,10 @@
+// function adicionar(a, b){
+//     return a + b;
+// }
+
+let total = 100;
+
+const adicionar = (a, b) => a + b;
+
+const resultado = adicionar(20, 40);    
+console.log(resultado); //CONSERTAR
