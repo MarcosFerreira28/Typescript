e77b12560c7ff919c4b7a85c5ed1f7553587bd1a
Marcos <mvmendesferreira@id.uff.br>
2025-08-02 20:11:22 -0300

    add: adicionando questão 7

---
+++ questao7/questao7.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+;
+const resposta1 = {
+    dados: "OK",
+    sucesso: true
+};
+const resposta2 = {
+    dados: [1, 2, 3],
+    sucesso: false
+};
+console.log(resposta1);
+console.log(resposta2);
+//# sourceMappingURL=questao7.js.map
