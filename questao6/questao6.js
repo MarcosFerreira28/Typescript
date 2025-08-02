63902b9eeecca5bfb05ba4aec7aba0a6505e3f0a
Marcos <mvmendesferreira@id.uff.br>
2025-08-02 14:35:20 -0300

    add: adicionando verificações de erro

--- questao6/questao6.js
+++ questao6/questao6.js
@@ -1,13 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function retornarElemento(array, index) {
-    if (index < 0 || index > array.length) {
-        // console.log("Esse index é inválido")
+    if (index < 0 || index >= array.length) {
         return `O index ${index} é inválido!`;
+    }
+    else if (array[index] === undefined) {
+        return "Não há nenhum elemento na lista nessa posição!";
     }
     return array[index];
 }
 console.log(retornarElemento([10, 20, 30], 1));
 console.log(retornarElemento(["a", "b", "c"], 0));
 console.log(retornarElemento(["a", "b", "c"], 6));
+console.log(retornarElemento(["a", , "c"], 1));
 //# sourceMappingURL=questao6.js.map
--- questao6/questao6.ts
+++ questao6/questao6.ts
@@ -1,10 +1,16 @@
-function retornarElemento<T>(array: Array<T>, index: number){
-    if (index < 0 || index > array.length){
+function retornarElemento<T>(array: Array<T>, index: number) : T | string{
+    if (index < 0 || index >= array.length){
         return `O index ${index} é inválido!`;
     }
+    else if (array[index] === undefined){
+        return "Não há nenhum elemento na lista nessa posição!"
+    }
+
     return array[index];
 }
 
 console.log(retornarElemento([10,20,30], 1))
 console.log(retornarElemento(["a", "b", "c"], 0))
 console.log(retornarElemento(["a", "b", "c"], 6))
+console.log(retornarElemento(["a", , "c"], 1))
+
